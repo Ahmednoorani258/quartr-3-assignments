ae4afcb3675317aff4820ed00a7c19ec00276cc1
ahmednoorani <ahmednoorani258@gmail.com>
2025-03-18 13:19:20 -0700

    step 1 completed (intro python)

---
+++ 04-assignments/assignment-00-to-05/00-intro-python/02-agreement-bot.py
@@ -0,0 +1,10 @@
+def main():
+    
+    favorite_animal = input("What's your favorite animal? ")
+    print(f"\n\nMy favorite animal is also {favorite_animal}!")
+
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
