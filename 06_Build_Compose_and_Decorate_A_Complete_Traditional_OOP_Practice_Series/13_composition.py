8a08af9528cecb3181c6c0f0ea8bdd21c06a9e14
ahmednoorani <ahmednoorani258@gmail.com>
2025-04-25 22:39:49 +0500

    step 11 & 12 completed

--- 06_Build_Compose_and_Decorate_A_Complete_Traditional_OOP_Practice_Series/13_composition.py
+++ 06_Build_Compose_and_Decorate_A_Complete_Traditional_OOP_Practice_Series/13_composition.py
@@ -3,3 +3,27 @@
 # Assignment:
 # Create a class Engine and a class Car. Use composition by passing an Engine object to the Car class during initialization. Access a method of the Engine class via the Car class.
 
+class Engine:
+
+    def start(self):
+        return "Engine started!"
+
+    def stop(self):
+        return "Engine stopped!"
+
+
+class Car:
+    def __init__(self, engine):
+        self.engine = engine
+
+    def start_engine(self):
+        return self.engine.start()
+
+    def stop_engine(self):
+        return self.engine.stop()
+
+
+engine = Engine()
+car = Car(engine)
+
+print(car.start_engine())
